6c58a099a0231f16f091954de7e3e5c39d8f8d0d
Tarta86 <56796683+Tarta86@users.noreply.github.com>
2025-05-02 15:47:12 +0200

    Revert "next fixx"
    This reverts commit a428c854e6927c844571170042cb10258e356d04.

--- SCATT_Dashboard.py
+++ SCATT_Dashboard.py
@@ -300,12 +300,9 @@ def metrics(sh, st_hold):
             Stability=stab,
             Hold_Speed=hold_speed,
             Timing_Angle=timing_angle,
-            X_Timing=xtime,
-            Y_Timing=ytime,
             Center_Distance=dist,
             Score=score
         ))
-
 
     df = pd.DataFrame(rows)
     # Serien & Overall
@@ -662,7 +659,6 @@ def ampelformat_colors(df, cols):
     return styles
 
 df_disp = all_metrics.loc[display_rows].copy()
-df_disp = df_disp.drop(columns=["X_Timing", "Y_Timing"], errors="ignore")
 df_disp[ampel_cols] = df_disp[ampel_cols].applymap(lambda x: round(x, 2) if pd.notna(x) else "")
 
 if color_coded:
